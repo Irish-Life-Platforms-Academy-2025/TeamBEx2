16093c2399d9d2af5498d17762a7029bb68d1873
Clive Wood <clive.wood@canadalife.co.uk>
2025-02-13 15:35:13 +0000

    Removed Hardcoding

--- private.tf
+++ private.tf
@@ -2,27 +2,27 @@ resource "azurerm_virtual_network" "privatevnet1" {
   name                = "privatevnet1"
   address_space       = ["10.0.0.0/16"]
   location            = "uksouth"
-  resource_group_name = "teamb-terraform-rg"
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "privatesubnet1" {
   name                 = "privatesubnet1"
-  resource_group_name  = "teamb-terraform-rg"
-  virtual_network_name = "privatevnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.privatevnet1.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "privatesubnet2" {
   name                 = "privatesubnet2"
-  resource_group_name  = "teamb-terraform-rg"
-  virtual_network_name = "privatevnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.privatevnet1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_subnet" "privatesubnet3" {
   name                 = "privatesubnet3"
-  resource_group_name  = "teamb-terraform-rg"
-  virtual_network_name = "privatevnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.privatevnet1.name
   address_prefixes     = ["10.0.3.0/24"]
 
 }
@@ -30,7 +30,7 @@ resource "azurerm_subnet" "privatesubnet3" {
 resource "azurerm_network_interface" "privatenic1" {
   name                = "privatenic1"
   location            = "uksouth"
-  resource_group_name = "teamb-terraform-rg"
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -42,7 +42,7 @@ resource "azurerm_network_interface" "privatenic1" {
 resource "azurerm_network_interface" "privatenic2" {
   name                = "privatenic2"
   location            = "uksouth"
-  resource_group_name = "teamb-terraform-rg"
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -54,7 +54,7 @@ resource "azurerm_network_interface" "privatenic2" {
 resource "azurerm_network_interface" "privatenic3" {
   name                = "privatenic3"
   location            = "uksouth"
-  resource_group_name = "teamb-terraform-rg"
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
