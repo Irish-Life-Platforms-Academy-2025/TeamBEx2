953233e1eaf82dea241d74d2dabb1dcd2056b575
Eoin Healy <eoin.healy@canadalifegroup.com>
2025-02-13 15:40:01 +0000

    public pips, nics and tags

--- variables.tf
+++ variables.tf
@@ -10,10 +10,22 @@ variable "prefix" {
 
 variable "public_address_space" {
   description = "CIDR for public address space"
-  default = "10.1.0.0/16"
+  default     = "10.1.0.0/16"
 }
 
 variable "resource_count" {
   description = "Number of resources to create"
   default     = 3
+}
+
+variable "vm_size" {
+  description = "VM Size"
+  default     = "Standard_DS1_v2"
+}
+
+variable "tag" {
+  description = "Tag to apply to all resources"
+  default = {
+    "DeployedBy" = "TeamB"
+  }
 }
