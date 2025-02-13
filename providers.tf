c6ac46c99e3ff53fe15fa471bbd23343078b6db6
Clive Wood <clive.wood@canadalife.co.uk>
2025-02-13 13:53:12 +0000

    Providers

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+ 
+  backend "azurerm" {
+    resource_group_name  = "teamb-stage-rg"
+    storage_account_name = "teambterraformsa"
+    container_name       = "states"
+    key                  = "teamb.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "168b5162-e625-42f1-994a-dfcfff0433bb"
+
+}
