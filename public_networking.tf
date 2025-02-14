2f7e78c8031b573577c505331e49f1804a3363d9
Eoin Healy <eoin.healy@canadalifegroup.com>
2025-02-14 13:12:04 +0000

    fixed local variable to priv address space

--- public_networking.tf
+++ public_networking.tf
@@ -2,7 +2,7 @@ locals {
   pub_split_cidr  = split(".", var.public_address_space)
   pub_subnet_base = "${local.pub_split_cidr[0]}.${local.pub_split_cidr[1]}"
 
-  priv_split_cidr  = split(".", var.priv_address_space)
+  priv_split_cidr  = split(".", var.private_address_space)
   priv_subnet_base = "${local.priv_split_cidr[0]}.${local.priv_split_cidr[1]}"
 }
 
