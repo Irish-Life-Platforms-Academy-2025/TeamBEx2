b6f6457b5018584463fe52bc51587a85a60cb67c
Clive Wood <clive.wood@canadalife.co.uk>
2025-02-14 14:21:38 +0000

    Rejig subnet, NIC and disk numbers

--- private.tf
+++ private.tf
@@ -6,29 +6,19 @@ resource "azurerm_virtual_network" "privatevnet1" {
   tags                = var.tag
 }
 
+resource "azurerm_subnet" "privatesubnet0" {
+  name                = "${var.prefix}-privatesubnet0"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.privatevnet1.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
 resource "azurerm_subnet" "privatesubnet1" {
   name                = "${var.prefix}-privatesubnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.privatevnet1.name
   address_prefixes     = ["10.0.1.0/24"]
-}
 
-resource "azurerm_network_security_group" "privatevnet1-nsg" {
-  name                = "${var.prefix}-private-network-security-group1"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = azurerm_network_interface.public_nics[0].private_ip_address
-    destination_address_prefix = "*"
-  }
 }
 
 resource "azurerm_subnet" "privatesubnet2" {
@@ -39,47 +29,16 @@ resource "azurerm_subnet" "privatesubnet2" {
 
 }
 
-resource "azurerm_network_security_group" "privatevnet2-nsg" {
-  name                = "${var.prefix}-private-network-security-group2"
+resource "azurerm_network_interface" "privatenic0" {
+  name                = "${var.prefix}-privatenic0"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tag
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = azurerm_network_interface.public_nics[1].private_ip_address
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_subnet" "privatesubnet3" {
-  name                = "${var.prefix}-privatesubnet3"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.privatevnet1.name
-  address_prefixes     = ["10.0.3.0/24"]
-
-}
-
-resource "azurerm_network_security_group" "privatevnet3-nsg" {
-  name                = "${var.prefix}-private-network-security-group3"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = azurerm_network_interface.public_nics[2].private_ip_address
-    destination_address_prefix = "*"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.privatesubnet0.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
@@ -109,17 +68,33 @@ resource "azurerm_network_interface" "privatenic2" {
   }
 }
 
-resource "azurerm_network_interface" "privatenic3" {
-  name                = "${var.prefix}-privatenic3"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tag
+resource "azurerm_linux_virtual_machine" "privatevm0" {
+  name                  = "${var.prefix}-privatevm0"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.privatenic0.id]
+  size                  = "Standard_DS1_v2"
+  tags                  = var.tag
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.privatesubnet3.id
-    private_ip_address_allocation = "Dynamic"
+  os_disk {
+    name                 = "myOsDiskvm0"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name  = "hostname0"
+  admin_username = var.vm_username
+  admin_password = var.vm_password
+
+  disable_password_authentication = false
+
 }
 
 resource "azurerm_linux_virtual_machine" "privatevm1" {
@@ -149,9 +124,10 @@ resource "azurerm_linux_virtual_machine" "privatevm1" {
 
   disable_password_authentication = false
 
+
 }
 
-resource "azurerm_linux_virtual_machine" "privatevm2" {
+  resource "azurerm_linux_virtual_machine" "privatevm2" {
   name                  = "${var.prefix}-privatevm2"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
@@ -173,36 +149,6 @@ resource "azurerm_linux_virtual_machine" "privatevm2" {
   }
 
   computer_name  = "hostname2"
-  admin_username = var.vm_username
-  admin_password = var.vm_password
-
-  disable_password_authentication = false
-
-
-}
-
-  resource "azurerm_linux_virtual_machine" "privatevm3" {
-  name                  = "${var.prefix}-privatevm3"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.privatenic3.id]
-  size                  = "Standard_DS1_v2"
-  tags                  = var.tag
-
-  os_disk {
-    name                 = "myOsDiskvm3"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-
-  computer_name  = "hostname3"
   admin_username = var.vm_username
   admin_password = var.vm_password
 
