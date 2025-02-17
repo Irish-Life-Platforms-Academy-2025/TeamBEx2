7ce68dab9bcf2ad9cad84ac4fe72055e97ba96da
Clive Wood <clive.wood@canadalife.co.uk>
2025-02-17 14:02:00 +0000

    Initial Setup

--- private.tf
+++ private.tf
@@ -1,39 +1,31 @@
 resource "azurerm_virtual_network" "privatevnet1" {
-  name                = "${var.prefix}-privatevnet1"
+  name                = "${local.prefix}-privatevnet1"
   address_space       = [var.private_address_space]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tag
+  tags                = local.tags
 }
 
 resource "azurerm_subnet" "privatesubnet0" {
-  name                 = "${var.prefix}-privatesubnet0"
+  name                 = "${local.prefix}-privatesubnet0"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.privatevnet1.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_subnet" "privatesubnet1" {
-  name                 = "${var.prefix}-privatesubnet1"
+  name                 = "${local.prefix}-privatesubnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.privatevnet1.name
   address_prefixes     = ["10.0.1.0/24"]
 
 }
 
-resource "azurerm_subnet" "privatesubnet2" {
-  name                 = "${var.prefix}-privatesubnet2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.privatevnet1.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-}
-
 resource "azurerm_network_interface" "privatenic0" {
-  name                = "${var.prefix}-privatenic0"
+  name                = "${local.prefix}-privatenic0"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tag
+  tags                = local.tags
 
   ip_configuration {
     name                          = "internal"
@@ -43,10 +35,10 @@ resource "azurerm_network_interface" "privatenic0" {
 }
 
 resource "azurerm_network_interface" "privatenic1" {
-  name                = "${var.prefix}-privatenic1"
+  name                = "${local.prefix}-privatenic1"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tag
+  tags                = local.tags
 
   ip_configuration {
     name                          = "internal"
@@ -55,29 +47,17 @@ resource "azurerm_network_interface" "privatenic1" {
   }
 }
 
-resource "azurerm_network_interface" "privatenic2" {
-  name                = "${var.prefix}-privatenic2"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tag
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.privatesubnet2.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
 
 resource "azurerm_linux_virtual_machine" "privatevm0" {
-  name                  = "${var.prefix}-privatevm0"
+  name                  = "${local.prefix}-privatevm0"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.privatenic0.id]
   size                  = var.vm_size
-  tags                  = var.tag
+  zone                  = 1
+  tags                  = local.tags
 
   os_disk {
-    name                 = "myOsDiskvm0"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
@@ -97,15 +77,15 @@ resource "azurerm_linux_virtual_machine" "privatevm0" {
 }
 
 resource "azurerm_linux_virtual_machine" "privatevm1" {
-  name                  = "${var.prefix}-privatevm1"
+  name                  = "${local.prefix}-privatevm1"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.privatenic1.id]
   size                  = var.vm_size
-  tags                  = var.tag
+  zone                  = 2
+  tags                  = local.tags
 
   os_disk {
-    name                 = "myOsDiskvm1"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
@@ -124,31 +104,4 @@ resource "azurerm_linux_virtual_machine" "privatevm1" {
   disable_password_authentication = false
 }
 
-resource "azurerm_linux_virtual_machine" "privatevm2" {
-  name                  = "${var.prefix}-privatevm2"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.privatenic2.id]
-  size                  = var.vm_size
-  tags                  = var.tag
-
-  os_disk {
-    name                 = "myOsDiskvm2"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-
-  computer_name  = "hostname2"
-  admin_username = var.vm_username
-  admin_password = var.vm_password
-
-  disable_password_authentication = false
-}
 
