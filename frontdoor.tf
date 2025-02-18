fc8b2f9e865122a502db66134c2972443b03d8ca
Eoin Healy <119070756+EoinHealy0@users.noreply.github.com>
2025-02-18 12:10:06 +0000

    fmt

--- frontdoor.tf
+++ frontdoor.tf
@@ -1,10 +1,10 @@
 resource "azurerm_cdn_frontdoor_profile" "fed_profile" {
   name                = "${local.prefix}-frontdoor-profile"
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name = "Standard_AzureFrontDoor"
+  sku_name            = "Standard_AzureFrontDoor"
 }
 
 resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
-    name = "${local.prefix}-frontdoor-endpoint"
-    cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fed_profile.id
+  name                     = "${local.prefix}-frontdoor-endpoint"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fed_profile.id
 }
