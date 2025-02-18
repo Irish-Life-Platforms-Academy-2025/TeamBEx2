737ac4c90ed93cc28c7759f9889a1f3f20d72b78
Eoin Healy <119070756+EoinHealy0@users.noreply.github.com>
2025-02-18 15:49:42 +0000

    added tags

--- dns.tf
+++ dns.tf
@@ -1,6 +1,7 @@
 resource "azurerm_dns_zone" "dns" {
   name                = "${local.prefix}.com"
   resource_group_name = azurerm_resource_group.rg.name
+  tags = local.tags
 }
 
 resource "azurerm_dns_a_record" "www_record" {
--- frontdoor.tf
+++ frontdoor.tf
@@ -2,6 +2,7 @@ resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
   name                = "${local.prefix}-frontdoor-profile"
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Standard_AzureFrontDoor"
+  tags = local.tags
 }
 
 resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
