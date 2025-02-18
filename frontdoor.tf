a3fad5122852df67bd6844d031332dc61b67f5d6
Eoin Healy <119070756+EoinHealy0@users.noreply.github.com>
2025-02-18 14:24:30 +0000

    fd using lb

--- frontdoor.tf
+++ frontdoor.tf
@@ -1,4 +1,4 @@
-resource "azurerm_cdn_frontdoor_profile" "fed_profile" {
+resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
   name                = "${local.prefix}-frontdoor-profile"
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Standard_AzureFrontDoor"
@@ -6,5 +6,43 @@ resource "azurerm_cdn_frontdoor_profile" "fed_profile" {
 
 resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
   name                     = "${local.prefix}-frontdoor-endpoint"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fed_profile.id
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
+  name                     = "${local.prefix}-frontdoor-og"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
+
+  session_affinity_enabled = true
+
+  load_balancing {
+    sample_size = 4
+    successful_samples_required = 3
+  }
+
+  health_probe {
+    path = "/"
+    protocol = "Http"
+    interval_in_seconds = 100
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
+  name                     = "${local.prefix}-frontdoor-o"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
+
+  certificate_name_check_enabled = false
+  host_name = azurerm_public_ip.lb_pip.ip_address
+  enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_route" "name" {
+  name = "${local.prefix}-frontdoor-route"
+  cdn_frontdoor_endpoint_id = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
+  cdn_frontdoor_origin_ids = [azurerm_cdn_frontdoor_origin.fd_origin.id]
+  
+  supported_protocols = ["Http"]
+  patterns_to_match = ["/*"]
+  https_redirect_enabled = false
 }
